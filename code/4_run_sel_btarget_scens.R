458ac88c7e223a3d22da990e977eb28aa92dd09c
kathryn.doering <kathryn.doering@noaa.gov>
2020-08-11 16:03:06 -0700

    update: simplify scenarios

--- code/4_run_sel_btarget_scens.R
+++ code/4_run_sel_btarget_scens.R
@@ -20,15 +20,15 @@ dir.create(runs_path)
 dir.create(mods_path)
 
 # define the scenarios ----
-scen_vals <- c(4.5, 5, 5.5) 
+scen_vals <- c(3.5, 4.5, 5.5) 
 names(scen_vals) <-   c("sel-low", "sel-med", "sel-hi")
-HCR_vals <- c(30, 40)
-names(HCR_vals)  <- c("HCR-30-10", "HCR-40-10")
+Btgt_vals <- c(0.4, 0.6)
+names(Btgt_vals)  <- c("Btgt_0.4", "Btgt_0.6")
 scenarios <- data.frame(
-  scen_name = c(paste0(names(scen_vals), "-", names(HCR_vals)[1]),
-                paste0(names(scen_vals), "-", names(HCR_vals)[2])),
+  scen_name = c(paste0(names(scen_vals), "-", names(Btgt_vals)[1]),
+                paste0(names(scen_vals), "-", names(Btgt_vals)[2])),
   OM_path = rep(c(file.path(mods_path, names(scen_vals))),times = 2),
-  EM_path = rep(c(file.path(mods_path, names(HCR_vals))), times = c(3,3))
+  EM_path = rep(c(file.path(mods_path, names(Btgt_vals))), times = c(3,3))
 )
 # manipulate the OM ctl file---- 
 
@@ -57,30 +57,33 @@ for (i in scen_vals) {
   r4ss::SS_plots(r4ss::SS_output(tmp_cod_path, verbose = FALSE, printstats = FALSE), verbose = FALSE)
 }
 
-# manipulate EM HCRs ----
+# manipulate EM Forecasting ----
 
-for (i in HCR_vals) {
-  tmp_scen_name <- names(HCR_vals)[HCR_vals == i]
+for (i in Btgt_vals) {
+  tmp_scen_name <- names(Btgt_vals)[Btgt_vals == i]
   tmp_cod_path <- file.path(mods_path, tmp_scen_name)
   file.copy(from = cod_mod_path, to = mods_path, recursive = TRUE)
   file.rename(from = file.path(mods_path, "cod"), to = tmp_cod_path)
   # make model read initial values from control file and not ss.par
   fore <- r4ss::SS_readforecast(file.path(tmp_cod_path, "forecast.ss"), 
                                 verbose = FALSE)
-  # manipulate HCRS.
+  # manipulate the forecasting file.
   fore$MSY <- 1
-  fore$Forecast <- 1
-  fore$ControlRuleMethod <- 1
-  fore$BforconstantF <- 0.01*i 
-  fore$BfornoF <- 0.1
-  fore$Flimitfraction <- 0.75
+  fore$SPRtarget <- 0.45
+  fore$Btarget <- i # differs between scenarios
+  fore$Forecast <- 3 # to just forecast based on a biomass target
+  fore$ControlRuleMethod <- 3 # doesn't really matter, b/c not using a control rule
+  fore$BforconstantF <- 0.03 # set low to avoid using 
+  fore$BfornoF <- 0.01 # set low to avoid using
+  fore$Flimitfraction <- 1 # no buffer
   r4ss::SS_writeforecast(fore, tmp_cod_path, verbose = FALSE, overwrite = TRUE)
+  file.remove(file.path(tmp_cod_path, "forecast.ss_new")) # to make sure it is not used.
 }
 
 # get the sampling scheme ----
-sample_struct <- SSMSE::create_sample_struct(dat = datfile_path, nyrs = 20)
+sample_struct <- SSMSE::create_sample_struct(dat = datfile_path, nyrs = 100)
 # modify
-sample_struct$lencomp <- data.frame(Yr = seq(105, 120, by = 5), 
+sample_struct$lencomp <- data.frame(Yr = seq(105, 200, by = 5), 
                                     Seas = sample_struct$lencomp$Seas,
                                     FltSvy = sample_struct$lencomp$FltSvy, 
                                     Sex = sample_struct$lencomp$Sex, 
@@ -96,7 +99,7 @@ call_SSMSE <- function(scen_name, OM_dir_path, EM_dir_path, sample_struct) {
                           EM_in_dir_vec = EM_dir_path,
                           MS_vec = "EM",       # The management strategy is specified in the EM
                           use_SS_boot_vec = TRUE, # use the SS bootstrap module for sampling
-                          nyrs_vec = 20,        # Years to project OM forward
+                          nyrs_vec = 100,        # Years to project OM forward
                           scope = 2,
                           nyrs_assess_vec = 5, # Years between assessments
                           rec_dev_pattern = "rand", # Don't use recruitment deviations
@@ -106,7 +109,7 @@ call_SSMSE <- function(scen_name, OM_dir_path, EM_dir_path, sample_struct) {
   out
 }
 future::plan(multiprocess) # run in serial b/c just 1 scenario
-# Run SSMSE using the variables in the data fram.e
+# Run SSMSE using the variables in the data frame
 output <- furrr::future_pmap(scenarios, 
                              ~call_SSMSE(scen_name = ..1, OM_dir_path = ..2, 
                                          EM_dir_path = ..3, 
@@ -115,56 +118,57 @@ output <- furrr::future_pmap(scenarios,
 # look at results ----
 summary <- SSMSE::SSMSE_summary_all(dir = "model_runs")
 
-summary <- list()
-summary$ts <- read.csv("model_runs/ss3sim_ts.csv")
-summary$scalar <- read.csv("model_runs/ss3sim_scalar.csv")
 
-SSB_df <- check_convergence(summary)
 
-summary$ts <- tidyr::separate(summary$ts,
-                              col = model_run,
-                              into = c(NA, "model_type"),
-                              remove = FALSE,
-                              sep = "_",
-                              extra = "drop")
-ggplot2::ggplot(data = subset(summary$ts, model_run %in% c("cod_OM", "cod_EM_120")),
-                ggplot2::aes(x = year, y = SpawnBio)) +
-  ggplot2::geom_vline(xintercept = 100, color = "gray") +
-  ggplot2::geom_line(ggplot2::aes(linetype = as.character(iteration), color = model_type))+
-  ggplot2::scale_color_manual(values = c("#D65F00", "black")) +
-  ggplot2::scale_linetype_manual(values = rep("solid", 100)) +
-  ggplot2::guides(linetype = FALSE) +
-  #ggplot2::facet_wrap(. ~ scenario) +
-  ggplot2::theme_classic()
+
+# summary <- list()
+# summary$ts <- read.csv("model_runs/ss3sim_ts.csv")
+# summary$scalar <- read.csv("model_runs/ss3sim_scalar.csv")
+
+summary$ts$model_type <- ifelse(grepl("_EM_", summary$ts$model_run), "EM", "OM")
+
+SSB_df <- check_convergence(summary, n_EMs = 20, max_yr = 200)
 
 # calculate performance metrics ----
 # look at catch in OM from yrs 101:120.
 # Keep it simple with total catch, avgcatch, sdcatch, and SSB in yr 120
 
 OM_metrics <- NULL
-for (i in c("sel-low", "sel-med","sel-hi")) { # scenarios$scen_name to make general
+for (i in scenarios$scen_name) { # scenarios$scen_name to make general
   OM_name <- grep("_OM$",
-                  list.dirs(file.path("model_runs", i, "1"), full.names = FALSE), 
+                  list.dirs(file.path("model_runs", i, "2"), full.names = FALSE), 
                   value = TRUE)
-  OM_dat <- file.path("model_runs", i, as.character(1:5), OM_name, "ss3.dat")
-  tot_catch <- unlist(lapply(OM_dat, function(x) get_total_catch(x)))
+  OM_dat <- file.path("model_runs", i, as.character(2), OM_name, "ss3.dat")
   avg_catch <- unlist(lapply(OM_dat, function(x) get_avg_catch(x)))
   catch_sd <- unlist(lapply(OM_dat, function(x) get_catch_sd(x)))
-  tmp_df <- data.frame(iteration = 1:5, scenario = i, tot_catch = tot_catch,
+  tmp_df <- data.frame(iteration = 2, scenario = i,
                        avg_catch = avg_catch, catch_sd = catch_sd)
   OM_metrics <- rbind(OM_metrics, tmp_df)
 }
-SSB_120 <- get_SSB_yr(summary, yr = 120)
+SSB_avg <- get_SSB_avg(summary, min_yr = 175, max_yr = 200)
 
-all_metrics <- full_join(OM_metrics, SSB_120)
-all_metrics_long <- tidyr::gather(all_metrics, "metric", "value", 3:6)
+all_metrics <- full_join(OM_metrics, SSB_avg)
+all_metrics_long <- tidyr::gather(all_metrics, "metric", "value", 3:5)
 all_metrics_long$value_bils <- all_metrics_long$value/1000000000
+all_metrics_long$scen_fac <- factor(all_metrics_long$scenario, 
+  levels = c("sel-low-Btgt_0.4", "sel-med-Btgt_0.4", "sel-hi-Btgt_0.4",
+             "sel-low-Btgt_0.6", "sel-med-Btgt_0.6", "sel-hi-Btgt_0.6" ))
 
-ggplot(data = all_metrics_long, aes(x = scenario, y = value_bils)) +
-  geom_boxplot() +
-  facet_wrap(~metric, scales = "free")+
-  scale_y_continuous(limits = c(0, NA))+
-  theme_classic()
+metrics <- unique(all_metrics_long$metric)
+# todo: convert to useing violin plots
+plots <- lapply(metrics, function(i, all_metrics_long) {
+  title_lab <- switch(i, 
+                  avg_catch = "Average Catch", 
+                  avg_SSB = "Average SSB", 
+                  catch_sd = "Catch SD")
+  plot <- ggplot(data = all_metrics_long[all_metrics_long$metric == i, ],
+         aes(x = scen_fac, y = value_bils)) +
+    geom_boxplot() +
+    scale_y_continuous(limits = c(0, NA))+
+    labs(title = title_lab, x = "Scenario", y = "Biomass (billions)") +
+    theme_classic()
+  plot
+}, all_metrics_long = all_metrics_long)
 
 ggsave(file.path("figures", "placeholder_plot.png"))
 
